397d8a671e60baec9c2b54ba617a55ea07faa101
lucasfmartins <52836194+lucasfmartins@users.noreply.github.com>
2020-10-07 10:25:25 -0300

    Add files via upload

---
+++ build/precache-manifest.e03b1ea8c1dc3ca720a87f0e37927418.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "015de887ffbdb57546a26256330c8a3b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c24869362f832e02b885",
+    "url": "/static/css/2.d34346ea.chunk.css"
+  },
+  {
+    "revision": "c24869362f832e02b885",
+    "url": "/static/js/2.51128bd2.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.51128bd2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "04a9534969ebd1c8be26",
+    "url": "/static/js/main.447fedfe.chunk.js"
+  },
+  {
+    "revision": "86623106b30129be8df8",
+    "url": "/static/js/runtime-main.73a6e729.js"
+  },
+  {
+    "revision": "e2a67d050636d4c2fff4a94d66c52991",
+    "url": "/static/media/background.e2a67d05.svg"
+  },
+  {
+    "revision": "ba534bc3deede8b52922fbe28c544059",
+    "url": "/static/media/lovo.ba534bc3.svg"
+  }
+]);
